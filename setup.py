b3f6c2f74c26e48dd014c824cf7b5792ce1f0218
Matias Roson <matias.roson@moni.com.ar>
2019-10-30 00:04:30 -0300

    Test configure

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gatsbyjs-cli",
-    version="0.0.2",
+    name="gatsbyjs-client",
+    version="0.0.3",
     author="Matias Roson",
     author_email="matiroson@gmail.com",
     description="Client to working gatsby docker image",
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/mjroson/gatsby-docker",
     packages=setuptools.find_packages(),
     entry_points={
-        'console_scripts': ['gatsbyjs=gatsbyjs-cli.run:main'],
+        'console_scripts': ['gatsbyjs=gatsbyjs_cli.run:main'],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
